e2cb8fe21290ea7d9ba997eab4f5ce3f30442b43
Ronal62 <ichojuraku@gmail.com>
2023-09-07 22:35:29 +0700

    Pemula

---
+++ index.py
@@ -0,0 +1,14 @@
+bilangan=[]
+r=int(input("How many that you wanna enter the number ? :"))
+for i in range(r):
+    angka=int=(input("enter the number:"))
+    bilangan.append(angka)
+    print("before sorting number: ", bilangan)
+    bilangan.sort()
+    print("after sorting number", bilangan)
+    
+
+
+
+    
+    
---
+++ main.py
@@ -0,0 +1,5 @@
+angka= [3,2,4,1]
+
+angka.sort()
+
+print(angka)
